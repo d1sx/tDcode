4e7cb0b1c47b6f60b2a66ce4fa6021862d569abe
d1sx_ <100376671+d1sx@users.noreply.github.com>
2022-02-25 16:50:46 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 pip3 install hashlib 2>/dev/null
 chmod +x main.py
 rm -rf ./*.sh
-printf "\nencode: python3 main.py\ndecode: python3 main.py {wordlist}\n"
+printf "\nencode: python3 tdcode.py\ndecode: python3 tdcode.py {wordlist}\n"
 
